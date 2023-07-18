847fc85a9acbd21f989b85d90908fcd1d6888497
Stuart Campbell <scampbell@bnl.gov>
2023-07-17 21:04:45 -0400

    Add more service functions
    facility_cycles -> list cycles for a facility
    beamline_by_name -> returns beamline given the name (TLA)

--- nsls2api/services/beamline_service.py
+++ nsls2api/services/beamline_service.py
@@ -1,5 +1,15 @@
+from typing import Optional
+
 from nsls2api.models.beamlines import Beamline
+
 
 async def beamline_count() -> int:
     return await Beamline.count()
+
+
+async def beamline_by_name(name: str) -> Optional[Beamline]:
+    # TODO: check that the input name looks sensible
+    # TODO: make this case insensitive
+    beamline = await Beamline.find_one(Beamline.name == name)
+    return beamline
 
--- nsls2api/services/facility_service.py
+++ nsls2api/services/facility_service.py
@@ -1,5 +1,24 @@
 from nsls2api.models.facilities import Facility
+from nsls2api.models.cycles import Cycle
 
 
 async def facilities_count() -> int:
+    """
+    Count the number of facilities in the database.
+
+    :return: The number of facilities.
+    """
     return await Facility.count()
+
+
+async def facility_cycles(facility: str):
+    """
+    Facility Cycles
+
+    This method retrieves the cycles for a given facility.
+
+    :param facility: The facility name (str).
+    :return: A list of cycles for the facility (list[Cycle]).
+    """
+    cycles = await Cycle.find(Cycle.facility == facility)
+    return cycles
