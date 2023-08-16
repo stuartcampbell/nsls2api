5e7c1e7d21c181092b68b94548189367fbc8db20
Stuart Campbell <scampbell@bnl.gov>
2023-08-16 15:45:29 -0400

    Code reformatting

--- nsls2api/api/models/recent_proposal_model.py
+++ nsls2api/api/models/recent_proposal_model.py
@@ -3,11 +3,13 @@ from typing import Optional
 
 import pydantic
 
+
 class RecentProposal(pydantic.BaseModel):
     proposal_id: str
     title: str
     updated: datetime.datetime
     instruments: Optional[list[str]]
+
 
 class RecentProposalsModel(pydantic.BaseModel):
     count: int
