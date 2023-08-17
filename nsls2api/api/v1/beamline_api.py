0cc91af012595045710d7157630305249fdc60ce
Stuart Campbell <scampbell@bnl.gov>
2023-08-16 22:26:50 -0400

    Change error handling to raise exception

--- nsls2api/api/v1/beamline_api.py
+++ nsls2api/api/v1/beamline_api.py
@@ -1,7 +1,7 @@
 import fastapi
 
+from fastapi import HTTPException
 from nsls2api.api.models.beamline_model import BeamlineServicesModel
-# from nsls2api.api.models.beamline_model import BeamlineResponseModel
 from nsls2api.models.beamlines import Beamline, BeamlineService
 from nsls2api.services import beamline_service
 
@@ -12,17 +12,15 @@ router = fastapi.APIRouter()
 async def details(name: str):
     beamline = await beamline_service.beamline_by_name(name)
     if beamline is None:
-        return fastapi.responses.JSONResponse({'error': f'Beamline named {name} could not be found'},
-                                              status_code=404)
+        raise HTTPException(status_code=404, detail=f'Beamline named {name} could not be found')
     return beamline
 
-@router.get('/beamline/{name}/services', response_model=BeamlineServicesModel)
+@router.get('/beamline/{name}/services', response_model=list[BeamlineService])
 async def get_beamline_services(name: str):
     beamline_services = await beamline_service.all_services(name)
     if beamline_services is None:
-        return fastapi.responses.JSONResponse({'error': f'Beamline named {name} could not be found'},
-                                              status_code=404)
+        raise HTTPException(status_code=404, detail=f'Beamline named {name} could not be found')
 
-    service_list = BeamlineServicesModel(count=2, services=*beamline_services)
+    # service_list = BeamlineServicesModel(count=2, services=beamline_services)
 
-    return service_list
+    return beamline_services
