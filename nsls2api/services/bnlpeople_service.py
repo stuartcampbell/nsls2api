8f77d0e35f3335c623da0fba93dd977bc2b545d0
Stuart Campbell <scampbell@bnl.gov>
2023-08-18 17:29:04 -0400

    Add BNL People API

---
+++ nsls2api/services/bnlpeople_service.py
@@ -0,0 +1,49 @@
+from .helpers import _call_async_webservice
+
+base_url = 'https://api.bnl.gov/BNLPeople'
+
+
+async def get_all_people_async():
+    url = f'{base_url}/api/BNLPeople'
+    people = await _call_async_webservice(url)
+    return people
+
+
+async def get_person_by_username_async(username: str):
+    url = f'{base_url}/api/BNLPeople?accountName={username}'
+    person = await _call_async_webservice(url)
+    return person
+
+
+async def get_person_by_id_async(lifenumber: str):
+    url = f'{base_url}/api/BNLPeople?employeeNumber={lifenumber}'
+    person = await _call_async_webservice(url)
+    return person
+
+
+async def get_person_by_email_async(email: str):
+    url = f'{base_url}/api/BNLPeople?email={email}'
+    person = await _call_async_webservice(url)
+    return person
+
+
+async def get_people_by_department_async(department_code: str):
+    url = f'{base_url}/api/BNLPeople?departmentCode={department_code}'
+    people = await _call_async_webservice(url)
+    return people
+
+
+async def get_people_by_status_async(status: str):
+    if status.title() not in ("Active", "Inactive", "Pending"):
+        raise ValueError("Status must be either 'Active', 'Inactive', 'Pending'")
+    url = f"{base_url}/api/BNLPeople?status={status}"
+    people = await _call_async_webservice(url)
+    return people
+
+
+async def get_people_by_calcstatus_async(calculated_status: str):
+    if calculated_status.title() not in ("Active", "Inactive"):
+        raise ValueError("Calculated Status must be either 'Active', 'Inactive'")
+    url = f'{base_url}/api/BNLPeople?status={calculated_status}'
+    people = await _call_async_webservice(url)
+    return people
