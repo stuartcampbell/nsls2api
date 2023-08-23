667dbba2420f41086c90c657044263b2310a0747
Stuart Campbell <scampbell@bnl.gov>
2023-08-23 17:39:44 -0400

    Add model for commissioning proposals

--- nsls2api/api/models/proposal_model.py
+++ nsls2api/api/models/proposal_model.py
@@ -7,14 +7,10 @@ class UsernamesModel(pydantic.BaseModel):
     usernames: list[str]
 
     model_config = {
-        "json_schema_extra": {
-            "examples": [
-                {
-                    "usernames": [
-                        "rdeckard",
-                        "rbatty"
-                    ]
-                }
-            ]
-        }
+        "json_schema_extra": {"examples": [{"usernames": ["rdeckard", "rbatty"]}]}
     }
+
+
+class CommissioningProposalsModel(pydantic.BaseModel):
+    count: int
+    commissioning_proposals: list[str]
