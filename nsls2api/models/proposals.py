c0bec1daffa9445b7b0fcff765c6a699cf6eb4f2
Stuart Campbell <scampbell@bnl.gov>
2023-08-23 17:40:27 -0400

    Add model for proposal_id only projections

--- nsls2api/models/proposals.py
+++ nsls2api/models/proposals.py
@@ -31,19 +31,39 @@ class Proposal(beanie.Document):
     cycles: Optional[list[str]]
     users: list[User]
     safs: Optional[list[SafetyForm]]
-    created_on: datetime.datetime = pydantic.Field(default_factory=datetime.datetime.now)
-    last_updated: datetime.datetime = pydantic.Field(default_factory=datetime.datetime.now)
+    created_on: datetime.datetime = pydantic.Field(
+        default_factory=datetime.datetime.now
+    )
+    last_updated: datetime.datetime = pydantic.Field(
+        default_factory=datetime.datetime.now
+    )
 
     class Settings:
-        name = 'proposals'
+        name = "proposals"
         indexes = [
-            pymongo.IndexModel(keys=[("proposal_id", pymongo.DESCENDING)], name="proposal_id_descend"),
-            pymongo.IndexModel(keys=[("last_updated", pymongo.DESCENDING)], name="last_updated_descend"),
-            pymongo.IndexModel(keys=[("users.username", pymongo.ASCENDING)], name="users_username_ascend"),
-            pymongo.IndexModel(keys=[("users.email", pymongo.ASCENDING)], name="users_email_ascend"),
-            pymongo.IndexModel(keys=[("users.bnl_id", pymongo.ASCENDING)], name="users_bnl_id_ascend"),
-            pymongo.IndexModel(keys=[("users.last_name", pymongo.ASCENDING)], name="users_last_name_ascend"),
-            pymongo.IndexModel(keys=[("safs.saf_id", pymongo.DESCENDING)], name="safs_saf_id_descend"),
+            pymongo.IndexModel(
+                keys=[("proposal_id", pymongo.DESCENDING)], name="proposal_id_descend"
+            ),
+            pymongo.IndexModel(
+                keys=[("last_updated", pymongo.DESCENDING)], name="last_updated_descend"
+            ),
+            pymongo.IndexModel(
+                keys=[("users.username", pymongo.ASCENDING)],
+                name="users_username_ascend",
+            ),
+            pymongo.IndexModel(
+                keys=[("users.email", pymongo.ASCENDING)], name="users_email_ascend"
+            ),
+            pymongo.IndexModel(
+                keys=[("users.bnl_id", pymongo.ASCENDING)], name="users_bnl_id_ascend"
+            ),
+            pymongo.IndexModel(
+                keys=[("users.last_name", pymongo.ASCENDING)],
+                name="users_last_name_ascend",
+            ),
+            pymongo.IndexModel(
+                keys=[("safs.saf_id", pymongo.DESCENDING)], name="safs_saf_id_descend"
+            ),
         ]
 
 
@@ -54,3 +74,10 @@ class UsernamesOnly(pydantic.BaseModel):
         projection = {
             "username": "$username",
         }
+
+
+class ProposalIdView(pydantic.BaseModel):
+    proposal_id: str
+
+    class Settings:
+        projection = {"proposal_id": "$proposal_id"}
