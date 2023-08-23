88561d341cc225889dd505a323b355d3ae411b36
Stuart Campbell <scampbell@bnl.gov>
2023-08-23 17:40:54 -0400

    Various build out of proposals endpoints

--- nsls2api/api/v1/proposal_api.py
+++ nsls2api/api/v1/proposal_api.py
@@ -1,20 +1,31 @@
 import fastapi
 
-from api.models.proposal_model import UsernamesModel
-from api.models.recent_proposal_model import RecentProposalsModel, RecentProposal
-from models.proposals import Proposal, User
-from services import proposal_service
+from nsls2api.api.models.proposal_model import UsernamesModel
+from nsls2api.api.models.recent_proposal_model import (
+    RecentProposalsModel,
+    RecentProposal,
+)
+from fastapi import Depends
+from nsls2api.models.proposals import Proposal, User
+from nsls2api.services import proposal_service
+
+from nsls2api.api.models.proposal_model import CommissioningProposalsModel
 
 router = fastapi.APIRouter()
 
 
-@router.get('/proposals/recent/{count}', response_model=RecentProposalsModel)
-async def recent(count: int, beamline: str | None = None):
+@router.get("/proposals/recent/{count}", response_model=RecentProposalsModel)
+async def get_recent_proposals(count: int, beamline: str | None = None):
     count = max(1, count)
     proposals = await proposal_service.recently_updated(count, beamline=beamline)
 
     proposal_models = [
-        RecentProposal(proposal_id=p.proposal_id, title=p.title, updated=p.last_updated, instruments=p.instruments)
+        RecentProposal(
+            proposal_id=p.proposal_id,
+            title=p.title,
+            updated=p.last_updated,
+            instruments=p.instruments,
+        )
         for p in proposals
     ]
     model = RecentProposalsModel(count=count, proposals=proposal_models)
@@ -22,40 +33,65 @@ async def recent(count: int, beamline: str | None = None):
     return model
 
 
-@router.get('/proposal/{proposal_id}', response_model=Proposal)
-async def details(proposal_id: str):
+@router.get("/proposals/commissioning", response_model=CommissioningProposalsModel)
+async def get_commissioning_proposals(beamline: str | None = None):
+    proposals = await proposal_service.commissioning_proposals(beamline=beamline)
+    if proposals is None:
+        return fastapi.responses.JSONResponse(
+            {"error": f"Proposal {proposal_id} not found"}, status_code=404
+        )
+    model = CommissioningProposalsModel(count=len(proposals), commissioning_proposals=proposals)
+    return model
+
+
+@router.get("/proposal/{proposal_id}", response_model=Proposal)
+async def get_proposal(proposal_id: int):
     proposal = await proposal_service.proposal_by_id(proposal_id)
     if proposal is None:
-        return fastapi.responses.JSONResponse({'error': f'Proposal {proposal_id} not found'}, status_code=404)
+        return fastapi.responses.JSONResponse(
+            {"error": f"Proposal {proposal_id} not found"}, status_code=404
+        )
     return proposal
 
 
-@router.get('/proposal/{proposal_id}/users', response_model=list[User])
-async def users(proposal_id: str):
+@router.get("/proposal/{proposal_id}/users", response_model=list[User])
+async def get_proposals_users(proposal_id: int):
     users = await proposal_service.users_from_proposal(proposal_id)
     return users
 
 
-@router.get('/proposal/{proposal_id}/pi', response_model=list[User])
-async def users(proposal_id: str):
+@router.get("/proposal/{proposal_id}/principle_invesigator", response_model=list[User])
+async def get_proposal_principle_invesigator(proposal_id: int):
     principle_invesigator = await proposal_service.pi_from_proposal(proposal_id)
 
     if len(principle_invesigator) == 0:
-        return fastapi.responses.JSONResponse({'error': f'PI not found for proposal {proposal_id}'}, status_code=404)
+        return fastapi.responses.JSONResponse(
+            {"error": f"PI not found for proposal {proposal_id}"}, status_code=404
+        )
     elif len(principle_invesigator) > 1:
-        return fastapi.responses.JSONResponse({'error': f'Proposal {proposal_id} contains more than one PI'},
-                                              status_code=422)
+        return fastapi.responses.JSONResponse(
+            {"error": f"Proposal {proposal_id} contains more than one PI"},
+            status_code=422,
+        )
 
     return principle_invesigator
 
 
-@router.get('/proposal/{proposal_id}/usernames', response_model=UsernamesModel)
-async def usernames(proposal_id: str):
+@router.get("/proposal/{proposal_id}/usernames", response_model=UsernamesModel)
+async def get_proposal_usernames(proposal_id: int):
     # Check to see if proposal exists
     if not await proposal_service.exists(proposal_id):
-        return fastapi.responses.JSONResponse({'error': f'Proposal {proposal_id} not found'}, status_code=404)
+        return fastapi.responses.JSONResponse(
+            {"error": f"Proposal {proposal_id} not found"}, status_code=404
+        )
 
     proposal_usernames = await proposal_service.usernames_from_proposal(proposal_id)
     model = UsernamesModel(usernames=proposal_usernames)
 
     return model
+
+
+@router.get("/proposal/{proposal_id}/directories")
+async def get_proposal_directories(proposal_id: int):
+
+    pass
--- nsls2api/services/proposal_service.py
+++ nsls2api/services/proposal_service.py
@@ -3,11 +3,11 @@ from typing import Optional
 from beanie.operators import In
 
 # from models.proposals import Proposal
-from nsls2api.models.proposals import Proposal, User
+from nsls2api.models.proposals import Proposal, User, ProposalIdView
 
 
-async def exists(proposal_id: str) -> bool:
-    proposal = await Proposal.find_one(Proposal.proposal_id == proposal_id)
+async def exists(proposal_id: int) -> bool:
+    proposal = await Proposal.find_one(Proposal.proposal_id == str(proposal_id))
     return False if proposal is None else True
 
 
@@ -15,7 +15,7 @@ async def proposal_count() -> int:
     return await Proposal.count()
 
 
-async def recently_updated(count=5, beamline: str | None=None):
+async def recently_updated(count=5, beamline: str | None = None):
     """
     Function to fetch recently updated proposals.
 
@@ -33,13 +33,23 @@ async def recently_updated(count=5, beamline: str | None=None):
         beamline = beamline.upper()
         # print(f"Searching for proposals within {beamline}...")
         query = In(Proposal.instruments, [beamline])
-        updated = await Proposal.find_many(query).sort(-Proposal.last_updated).limit(count).to_list()
+        updated = (
+            await Proposal.find_many(query)
+            .sort(-Proposal.last_updated)
+            .limit(count)
+            .to_list()
+        )
     else:
-        updated = await Proposal.find_all().sort(-Proposal.last_updated).limit(count).to_list()
+        updated = (
+            await Proposal.find_many()
+            .sort(-Proposal.last_updated)
+            .limit(count)
+            .to_list()
+        )
     return updated
 
 
-async def proposal_by_id(proposal_id: str) -> Optional[Proposal]:
+async def proposal_by_id(proposal_id: int) -> Optional[Proposal]:
     """
     Retrieve a proposal by its ID.
 
@@ -49,37 +59,45 @@ async def proposal_by_id(proposal_id: str) -> Optional[Proposal]:
 
     if not proposal_id:
         return None
-    proposal_id = proposal_id.lower().strip()
 
-    proposal: Proposal = await Proposal.find_one(Proposal.proposal_id == proposal_id)
+    # proposal_id = proposal_id.strip()
+    proposal: Proposal = await Proposal.find_one(Proposal.proposal_id == str(proposal_id))
+
+    print(proposal)
+
     return proposal
 
 
-async def users_from_proposal(proposal_id: str) -> Optional[list[User]]:
+async def users_from_proposal(proposal_id: int) -> Optional[list[User]]:
     proposal = await proposal_by_id(proposal_id)
     return proposal.users
 
 
-async def usernames_from_proposal(proposal_id: str) -> Optional[list[str]]:
+async def usernames_from_proposal(proposal_id: int) -> Optional[list[str]]:
     proposal = await proposal_by_id(proposal_id)
 
     if proposal is None:
-        raise Exception(f'Proposal {proposal_id} not found')
+        raise Exception(f"Proposal {proposal_id} not found")
 
-    usernames = [
-        u.username
-        for u in proposal.users if u.username is not None
-    ]
+    usernames = [u.username for u in proposal.users if u.username is not None]
     return usernames
 
 
-async def pi_from_proposal(proposal_id: str) -> Optional[list[User]]:
+async def pi_from_proposal(proposal_id: int) -> Optional[list[User]]:
     proposal = await proposal_by_id(proposal_id)
 
-    pi = [
-        u for u in proposal.users if u.is_pi
-    ]
+    pi = [u for u in proposal.users if u.is_pi]
 
-    # TODO: Check for multiple of no PIs
+    # TODO: Check for multiple or no PIs
 
     return pi
+
+
+async def commissioning_proposals(beamline: str | None = None):
+    proposals = (
+        Proposal.find_many(Proposal.pass_type_id == "300005", projection_model=ProposalIdView)
+    )
+
+    commissioning_proposal_list = [p.proposal_id for p in await proposals.to_list() if p.proposal_id is not None]
+
+    return commissioning_proposal_list
