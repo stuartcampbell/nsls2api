2e7858a987e206f7da26f9114403b9c4ff373a02
Stuart Campbell <scampbell@bnl.gov>
2023-08-28 17:24:08 -0400

    use variable for pass api_key

--- nsls2api/services/pass_service.py
+++ nsls2api/services/pass_service.py
@@ -6,52 +6,50 @@ from httpx import Response
 from ..infrastructure import settings
 from .helpers import _call_async_webservice
 
-api_key: Optional[str] = None
-
+api_key: settings.PASS_API_KEY
 base_url = settings.PASS_API_URL
 
-
 async def get_proposal(proposal_id: int):
-    url = f'{base_url}/Proposal/GetProposal/{settings.PASS_API_KEY}/NSLS-II/{proposal_id}'
+    url = f'{base_url}/Proposal/GetProposal/{api_key}/NSLS-II/{proposal_id}'
     print(url)
     proposal = await _call_async_webservice(url)
     return proposal
 
 
 async def get_saf_from_proposal(proposal_id: int):
-    url = f'{base_url}/SAF/GetSAFsByProposal/{settings.PASS_API_KEY}/NSLS-II/{proposal_id}'
+    url = f'{base_url}/SAF/GetSAFsByProposal/{api_key}/NSLS-II/{proposal_id}'
     print(url)
     saf = await _call_async_webservice(url)
     return saf
 
 
 async def get_commissioning_proposals_by_year(year: int):
-    url = f"{base_url}Proposal/GetProposalsByType/{settings.PASS_API_KEY}/NSLS-II/{year}/300005/NULL"
+    url = f"{base_url}Proposal/GetProposalsByType/{api_key}/NSLS-II/{year}/300005/NULL"
     proposals = await _call_async_webservice(url)
     return proposals
 
 
-async def get_pass_resources_async():
-    url = f'{base_url}/Resource/GetResources/{settings.PASS_API_KEY}/NSLS-II'
+async def get_pass_resources():
+    url = f'{base_url}/Resource/GetResources/{api_key}/NSLS-II'
     resources = await _call_async_webservice(url)
     return resources
 
 
-async def get_cycles_async():
-    url = f'{base_url}/Proposal/GetCycles/{settings.PASS_API_KEY}/NSLS-II'
+async def get_cycles():
+    url = f'{base_url}/Proposal/GetCycles/{api_key}/NSLS-II'
     print(url)
     cycles = await _call_async_webservice(url)
     return cycles
 
 
-async def get_proposals_allocated_async():
-    url = f'{base_url}/Proposal/GetProposalsAllocated/{settings.PASS_API_KEY}/NSLS-II'
+async def get_proposals_allocated():
+    url = f'{base_url}/Proposal/GetProposalsAllocated/{api_key}/NSLS-II'
     allocated_proposals = await _call_async_webservice(url)
     return allocated_proposals
 
 
 async def get_proposals_by_person(bnl_id: str):
-    url = f'{base_url}/Proposal/GetProposalsByPerson/{settings.PASS_API_KEY}/NSLS-II/null/null/{bnl_id}/null'
+    url = f'{base_url}/Proposal/GetProposalsByPerson/{api_key}/NSLS-II/null/null/{bnl_id}/null'
     print(url)
     proposals = await _call_async_webservice(url)
     return proposals
