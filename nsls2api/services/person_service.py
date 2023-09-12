e0a6005a33f45b825793fb60e998a9bd9f62cd55
Stuart Campbell <scampbell@bnl.gov>
2023-09-12 08:26:51 -0400

    changed to async function

--- nsls2api/services/person_service.py
+++ nsls2api/services/person_service.py
@@ -4,7 +4,7 @@ from typing import Optional
 from nsls2api.api.models.person_model import Person
 
 
-def diagnostic_details_by_username(username: str) -> Optional[Person]:
+async def diagnostic_details_by_username(username: str) -> Optional[Person]:
     # TODO: Update this with some real person that we look up!
 
     person = Person(firstname="James", lastname="Moriarty", email="prof@crime.com", bnl_id="X6666X")
