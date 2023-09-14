db6725fe3ea5ef64bb47a686bbd823e96778aaae
Stuart Campbell <scampbell@bnl.gov>
2023-09-14 08:33:52 -0400

    Moved around some imports

--- nsls2api/infrastructure/mongodb_setup.py
+++ nsls2api/infrastructure/mongodb_setup.py
@@ -1,14 +1,15 @@
 import beanie
 import motor.motor_asyncio
 from rich.pretty import pprint
-import nsls2api.models
+from fastapi import Depends
+
+from nsls2api import models
 
 
 async def init_connection(hostname: str, port: int, db_name: str):
     conn_str = f"mongodb://{hostname}:{port}/{db_name}"
     client = motor.motor_asyncio.AsyncIOMotorClient(conn_str)
 
-    from nsls2api import models
     await beanie.init_beanie(database=client[db_name], document_models=models.all_models)
 
     pprint(f"Connected to {db_name} on localhost.")
