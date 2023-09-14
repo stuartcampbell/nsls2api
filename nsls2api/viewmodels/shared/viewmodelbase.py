550ef33441256e67a1a3733e3677d2a19b292ee5
Stuart Campbell <scampbell@bnl.gov>
2023-09-14 18:44:52 -0400

    Extract HTMX request header

--- nsls2api/viewmodels/shared/viewmodelbase.py
+++ nsls2api/viewmodels/shared/viewmodelbase.py
@@ -7,7 +7,10 @@ class ViewModelBase:
 
     def __init__(self, request: Request):
         self.request: Request = request
+        self.is_htmx_request = request.headers.get('HX-Request')
+
         self.error: Optional[str] = None
+        self.view_model = self.to_dict()
 
     def to_dict(self) -> dict:
         return self.__dict__
