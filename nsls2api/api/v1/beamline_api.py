8ecd176bf945ceb721b494f2241548126d53bbc6
Stuart Campbell <scampbell@bnl.gov>
2023-09-14 18:41:45 -0400

    Code format and cleanup imports

--- nsls2api/api/v1/beamline_api.py
+++ nsls2api/api/v1/beamline_api.py
@@ -1,8 +1,6 @@
 import fastapi
-
 from fastapi import HTTPException, Depends
 from fastapi.security.api_key import APIKey
-from nsls2api.infrastructure.security import get_api_key
 
 from nsls2api.infrastructure.security import get_api_key
 from nsls2api.models.beamlines import Beamline, BeamlineService
@@ -18,12 +16,14 @@ async def details(name: str):
         raise HTTPException(status_code=404, detail=f'Beamline named {name} could not be found')
     return beamline
 
+
 @router.get('/beamline/{name}/services', response_model=list[BeamlineService])
 async def get_beamline_services(name: str):
     beamline_services = await beamline_service.all_services(name)
     if beamline_services is None:
         raise HTTPException(status_code=404, detail=f'Beamline named {name} could not be found')
     return beamline_services
+
 
 @router.get('/beamline/{name}/accounts')
 async def get_beamline_accounts(name: str, api_key: APIKey = Depends(get_api_key)):
@@ -33,35 +33,39 @@ async def get_beamline_accounts(name: str, api_key: APIKey = Depends(get_api_key
     return service_accounts
 
 
-@router.get('/beamline/{name}/accounts/workflow',response_model=str)
+@router.get('/beamline/{name}/accounts/workflow', response_model=str)
 async def get_beamline_workflow_username(name: str):
     workflow_user = await beamline_service.workflow_username(name)
     if workflow_user is None:
         raise HTTPException(status_code=404, detail=f'No workflow user has been defined for the {name} beamline')
     return workflow_user
 
-@router.get('/beamline/{name}/accounts/ioc',response_model=str)
+
+@router.get('/beamline/{name}/accounts/ioc', response_model=str)
 async def get_beamline_ioc_username(name: str):
     ioc_user = await beamline_service.ioc_username(name)
     if ioc_user is None:
         raise HTTPException(status_code=404, detail=f'No IOC user has been defined for the {name} beamline')
     return ioc_user
 
-@router.get('/beamline/{name}/accounts/bluesky',response_model=str)
+
+@router.get('/beamline/{name}/accounts/bluesky', response_model=str)
 async def get_beamline_bluesky_username(name: str):
     bluesky_user = await beamline_service.bluesky_username(name)
     if bluesky_user is None:
         raise HTTPException(status_code=404, detail=f'No bluesky user has been defined for the {name} beamline')
     return bluesky_user
 
-@router.get('/beamline/{name}/accounts/epics-services',response_model=str)
+
+@router.get('/beamline/{name}/accounts/epics-services', response_model=str)
 async def get_beamline_epics_services_username(name: str):
     epics_user = await beamline_service.epics_services_username(name)
     if epics_user is None:
         raise HTTPException(status_code=404, detail=f'No EPICS services user has been defined for the {name} beamline')
     return epics_user
 
-@router.get('/beamline/{name}/accounts/operator',response_model=str)
+
+@router.get('/beamline/{name}/accounts/operator', response_model=str)
 async def get_beamline_operator_username(name: str):
     operator_user = await beamline_service.operator_username(name)
     if operator_user is None:
