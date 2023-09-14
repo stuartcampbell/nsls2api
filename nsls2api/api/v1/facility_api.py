c0e50663edf23c557eb6c1c6fa15384dd4b991d8
Stuart Campbell <scampbell@bnl.gov>
2023-09-14 18:41:25 -0400

    Temporary endpoint to return current cycle for NSLS-II

--- nsls2api/api/v1/facility_api.py
+++ nsls2api/api/v1/facility_api.py
@@ -2,3 +2,7 @@ import fastapi
 
 router = fastapi.APIRouter()
 
+
+@router.get('/facility/{facility}/cycles/current', response_model=str)
+async def get_current_facilty_cycle(facility: str):
+    return "2023-3"
