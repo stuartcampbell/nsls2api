cbf1184c5e10eab2c8e71c5eb71a30823b2e461b
Stuart Campbell <scampbell@bnl.gov>
2023-09-14 18:45:05 -0400

    WIP Proposals search

--- nsls2api/views/home.py
+++ nsls2api/views/home.py
@@ -1,8 +1,14 @@
 import fastapi
+import jinja_partials
 from starlette.requests import Request
 from starlette.templating import Jinja2Templates
 
+from nsls2api.viewmodels.proposals.details_viewmodel import DetailsViewModel
+from nsls2api.viewmodels.proposals.search_viewmodel import SearchViewModel
+
 templates = Jinja2Templates('templates')
+jinja_partials.register_starlette_extensions(templates)
+
 router = fastapi.APIRouter()
 
 
@@ -12,7 +18,28 @@ def index(request: Request):
     return templates.TemplateResponse('home/index.html', data)
 
 
+@router.get("/proposals/search", include_in_schema=False)
+async def search_proposals(request: Request):
+    vm = SearchViewModel(request)
+    await vm.load()
+
+    print(f"Searching for {vm.search_text}")
+    if vm.is_htmx_request:
+        return templates.TemplateResponse('shared/partials/search_results.html', vm.to_dict())
+
+    return templates.TemplateResponse('home/proposals_search.html', vm.to_dict())
+
+
+@router.get('/proposals', include_in_schema=False)
+async def proposals(request: Request):
+    vm = DetailsViewModel(311130, request)
+    await vm.load()
+
+    print(vm.to_dict())
+
+    return templates.TemplateResponse('home/proposals.html', vm.to_dict())
+
+
 @router.get('/favicon.ico', include_in_schema=False)
 def favicon():
     return fastapi.responses.RedirectResponse(url='/static/images/favicon.ico')
-
