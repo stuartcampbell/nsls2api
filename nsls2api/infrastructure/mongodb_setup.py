5c6a1c2b30e7bd1c7bc48f9f7dab3ff0bbbf4055
Stuart Campbell <scampbell@bnl.gov>
2023-10-04 22:13:58 -0400

    Add debugging startup message

--- nsls2api/infrastructure/mongodb_setup.py
+++ nsls2api/infrastructure/mongodb_setup.py
@@ -7,8 +7,10 @@ from nsls2api import models
 
 
 async def init_connection(mongodb_dsn: MongoDsn):
-    client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_dsn, uuidRepresentation="standard")
 
+    pprint(f"Attempting to connect to {str(mongodb_dsn)}")
+
+    client = motor.motor_asyncio.AsyncIOMotorClient(mongodb_dsn, uuidRepresentation="standard")
     await beanie.init_beanie(database=client.get_default_database(), document_models=models.all_models)
 
     pprint(f"Connected to {client.get_default_database().name} on localhost.")
