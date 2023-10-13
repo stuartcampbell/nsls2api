d71a144ca744c8805e12c8b877c9e74f00607ded
Stuart Campbell <scampbell@bnl.gov>
2023-10-13 09:26:50 -0400

    Code reformatting and fix spelling errors

--- nsls2api/api/v1/admin_api.py
+++ nsls2api/api/v1/admin_api.py
@@ -4,28 +4,34 @@ import fastapi
 from fastapi import Depends
 
 from nsls2api.infrastructure import config
-from nsls2api.infrastructure.security import validate_admin_role, generate_api_key
+from nsls2api.infrastructure.security import (
+    validate_admin_role,
+    generate_api_key,
+)
 from nsls2api.models.apikeys import ApiUser
 
 # router = fastapi.APIRouter()
 router = fastapi.APIRouter(dependencies=[Depends(validate_admin_role)])
 
 
-@router.get("/admin/settings")#, include_in_schema=False)
+@router.get("/admin/settings")  # , include_in_schema=False)
 async def info(
     settings: Annotated[config.Settings, Depends(config.get_settings)]
 ):
     return settings
-@router.get('/admim/validate', response_model=str)
-async def check_admin_validation(admin_user: Annotated[ApiUser, Depends(validate_admin_role)] = None):
+
+
+@router.get("/admin/validate", response_model=str)
+async def check_admin_validation(
+    admin_user: Annotated[ApiUser, Depends(validate_admin_role)] = None
+):
     """
-    :param admin_user: - The admin user to be checked for validation.
     :return: str - The username of the validated admin user.
     """
     return admin_user.username
 
 
-@router.post('/admin/generate_api_key/{username}')
+@router.post("/admin/generate_api_key/{username}")
 async def generate_user_apikey(username: str):
     """
     Generate an API key for a given username.
@@ -34,8 +40,3 @@ async def generate_user_apikey(username: str):
     :return: The generated API key.
     """
     return await generate_api_key(username)
-
-
-
-# The following endpoints are 'pass-through' routes to underlying services
-# They are here for convenience only
