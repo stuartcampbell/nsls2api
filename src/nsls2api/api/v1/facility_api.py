86a3715b90dc0c391369e5e99fa9f0e98113decf
Stuart Campbell <scampbell@bnl.gov>
2024-01-10 09:12:30 -0500

    Exclude current facility cycle endpoint from schema

--- src/nsls2api/api/v1/facility_api.py
+++ src/nsls2api/api/v1/facility_api.py
@@ -4,7 +4,7 @@ from nsls2api.api.models.facility_model import FacilityName
 
 router = fastapi.APIRouter()
 
-
-@router.get("/facility/{facility}/cycles/current", response_model=str)
+# TODO: Add back into schema when implemented.
+@router.get("/facility/{facility}/cycles/current", response_model=str, include_in_schema=False)
 async def get_current_facilty_cycle(facility: FacilityName):
     return "2023-3"
