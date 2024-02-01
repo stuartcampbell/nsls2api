1490e774dfdedf8b60bd7a263e98136adbe0d207
Stuart Campbell <scampbell@bnl.gov>
2024-02-01 14:44:05 -0500

    Add new API endpoint for retrieving current operating cycle

--- src/nsls2api/api/models/facility_model.py
+++ src/nsls2api/api/models/facility_model.py
@@ -9,3 +9,7 @@ class FacilityName(Enum):
 class FacilityCyclesResponseModel(pydantic.BaseModel):
     facility: str
     cycles: list[str]
+
+class FacilityCurrentOperatingCycleResponseModel(pydantic.BaseModel):
+    facility: str
+    cycle: str
