7121bce32ae8d17f68e5ed8ef591bd40ba9ecf90
Stuart Campbell <scampbell@bnl.gov>
2024-02-02 16:02:19 -0500

    Add AssetDirectoryGranularity enum for beamline API response models

--- src/nsls2api/api/models/beamline_model.py
+++ src/nsls2api/api/models/beamline_model.py
@@ -1,1 +1,15 @@
 # This is for response models relating to the beamline api endpoints.
+
+from enum import Enum
+
+
+class AssetDirectoryGranularity(Enum):
+    """
+    Represents the granularity options for asset directory YYYY/MM/DD/HH tree structure.
+    The value specifies the most granular level to create directories for.
+    """
+
+    year = "year"
+    month = "month"
+    day = "day"
+    hour = "hour"
