db0da263df62287d42a059c2af5928cf5e426699
Stuart Campbell <scampbell@bnl.gov>
2024-02-05 16:59:03 -0500

    Refactor main.py: Remove unused imports and middleware, configure routing

--- src/nsls2api/main.py
+++ src/nsls2api/main.py
@@ -1,12 +1,8 @@
 from pathlib import Path
 
 import fastapi
-import logging
 import uvicorn
-
 from asgi_correlation_id import CorrelationIdMiddleware
-
-from starlette.exceptions import HTTPException
 from starlette.middleware import Middleware
 from starlette.staticfiles import StaticFiles
 
@@ -18,30 +14,10 @@ from nsls2api.api.v1 import stats_api as stats_api_v1
 from nsls2api.api.v1 import user_api as user_api_v1
 from nsls2api.infrastructure import mongodb_setup
 from nsls2api.infrastructure.config import get_settings
-from nsls2api.infrastructure.logging import configure_logging
-from nsls2api.views import diagnostics
-from nsls2api.views import home
 from nsls2api.middleware import ProcessTimeMiddleware
-from nsls2api.middleware import log_request
-from fastapi.exceptions import RequestValidationError
-from nsls2api.exception_handlers import request_validation_exception_handler, http_exception_handler, unhandled_exception_handler
-
+from nsls2api.views import diagnostics, home
 
 settings = get_settings()
-
-logger = logging.getLogger(__name__)
-
-middleware = [Middleware(ProcessTimeMiddleware)]
-
-app = fastapi.FastAPI(title="NSLS-II API", middleware=middleware, on_startup=[configure_logging])
-app.add_middleware(CorrelationIdMiddleware)
-
-app.middleware("http")(log_request)
-
-app.add_exception_handler(RequestValidationError, request_validation_exception_handler)
-app.add_exception_handler(HTTPException, http_exception_handler)
-app.add_exception_handler(Exception, unhandled_exception_handler)
-
 
 current_file = Path(__file__)
 current_file_dir = current_file.parent
@@ -50,6 +26,11 @@ project_root = current_file_dir.parent
 project_root_absolute = project_root.resolve()
 static_root_absolute = current_file_dir_absolute / "static"
 
+middleware = [Middleware(ProcessTimeMiddleware)]
+
+app = fastapi.FastAPI(title="NSLS-II API", middleware=middleware)
+app.add_middleware(CorrelationIdMiddleware)
+
 
 def main():
     configure_routing()
@@ -57,7 +38,6 @@ def main():
 
 
 def configure_routing():
-    logger.info("Configuring routing")
     app.include_router(proposal_api_v1.router, prefix="/v1", tags=["proposal"])
     app.include_router(stats_api_v1.router, prefix="/v1")
     app.include_router(beamline_api_v1.router, prefix="/v1", tags=["beamline"])
