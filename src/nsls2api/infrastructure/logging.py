34ff8c0d54f1a725d9c5ccaf5d07c6d49111bc0c
Stuart Campbell <scampbell@bnl.gov>
2024-02-05 16:54:48 -0500

    Get handle to uvicorn logger

--- src/nsls2api/infrastructure/logging.py
+++ src/nsls2api/infrastructure/logging.py
@@ -1,47 +1,7 @@
 import logging
-from logging.config import dictConfig
-from asgi_correlation_id import CorrelationIdFilter
-    
-# Disable uvicorn access logger
-uvicorn_access = logging.getLogger("uvicorn.access")
-uvicorn_access.disabled = True
+import nsls2api
 
-logger = logging.getLogger("uvicorn")
-logger.setLevel(logging.getLevelName(logging.DEBUG))
+# Grab the uvicorn logger so we can generate logs to the application log
+logger = logging.getLogger(f"uvicorn.error.{nsls2api.__name__}")
 
-def configure_logging() -> None:
-    dictConfig(
-        {
-            'version': 1,
-            'disable_existing_loggers': False,
-            'filters': {
-                'correlation_id': {
-                    '()': CorrelationIdFilter,
-                    'uuid_length': 32,
-                    'default_value': '-'
-                },
-            },
-            'formatters': {
-                'console': {
-                    'class': 'logging.Formatter',
-                    'datefmt': '%H:%M:%S',
-                    'format': '%(levelname)s: \t  %(asctime)s %(name)s:%(lineno)d [%(correlation_id)s] %(message)s',
-                },
-            },
-            'handlers': {
-                'console': {
-                    'class': 'logging.StreamHandler',
-                    'filters': ['correlation_id'],
-                    'formatter': 'console',
-                },
-            },
-            'loggers': {
-                # project
-                'app': {'handlers': ['console'], 'level': 'DEBUG', 'propagate': True},
-                # third-party packages
-                'httpx': {'handlers': ['console'], 'level': 'INFO'},
-                'databases': {'handlers': ['console'], 'level': 'WARNING'},
-                'asgi_correlation_id': {'handlers': ['console'], 'level': 'WARNING'},
-            },
-        }
-    )
+
