b3625d15437fdb3acfef9b7bbb9aee8497a095ec
Stuart Campbell <scampbell@bnl.gov>
2024-04-09 09:29:01 -0400

    Migrate mongodb setup to lifecycle function

---
+++ src/nsls2api/infrastructure/app_setup.py
@@ -0,0 +1,31 @@
+import asyncio
+from contextlib import asynccontextmanager
+
+from nsls2api.infrastructure import mongodb_setup
+from nsls2api.infrastructure.config import get_settings
+from nsls2api.services import background_service
+
+settings = get_settings()
+
+development_mode = True
+
+
+@asynccontextmanager
+async def app_lifespan(_):
+    if development_mode:
+        # Default to local mongodb with default port 
+        # and no authentication for development.
+        # TODO: Refactor to use a different database for development.
+        await mongodb_setup.init_connection(settings.mongodb_dsn.unicode_string())
+    else:
+        await mongodb_setup.init_connection(settings.mongodb_dsn.unicode_string())
+
+
+    # Start the background workers
+
+    # noinspection PyAsyncCall
+    asyncio.create_task(background_service.background_worker())
+    
+    yield
+
+    # Nothing to clean up. 
