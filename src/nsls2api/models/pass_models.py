0e6c40154bbea310751f14db1388cb7f6d6d7af7
Stuart Campbell <scampbell@bnl.gov>
2024-04-09 09:30:45 -0400

    Refactor PASS models to improve code organization and readability

--- src/nsls2api/models/pass_models.py
+++ src/nsls2api/models/pass_models.py
@@ -2,10 +2,31 @@ from typing import Optional
 import pydantic
 
 
+class PassPerson(pydantic.BaseModel):
+    """
+    This class represents PASS's representation of a Person (e.g. PI or Creator).
+    """
+
+    Can_Edit: Optional[bool] = None
+    Can_Read: Optional[bool] = None
+    CoPI: Optional[bool] = None
+    On_Site: Optional[bool] = None
+    Pool_ID: Optional[int] = None
+    Proposal_ID: Optional[int] = None
+    User_ID: Optional[int] = None
+    Account: Optional[str] = None
+    BNL_ID: Optional[str] = None
+    Email: Optional[str] = None
+    First_Name: Optional[str] = None
+    Last_Name: Optional[str] = None
+    User_Faciity_ID: Optional[str] = None
+
+
 class PassAllocation(pydantic.BaseModel):
     """
     This class represents PASS's representation of an Allocation.
     """
+
     Expired: Optional[bool] = None
     Expiration_Date: Optional[str] = None
     Allocated_Proposal_Type_ID: Optional[int] = None
@@ -28,11 +49,12 @@ class PassAllocation(pydantic.BaseModel):
     Creator: Optional[PassPerson] = None
     PI: Optional[PassPerson] = None
 
-    
+
 class PassCycle(pydantic.BaseModel):
     """
     This class represents PASS's representation of a Cycle.
     """
+
     Active: Optional[bool] = None
     ID: Optional[int] = None
     Year: Optional[int] = None
@@ -43,28 +65,11 @@ class PassCycle(pydantic.BaseModel):
     User_Facility_ID: Optional[str] = None
 
 
-class PassPerson(pydantic.BaseModel):
-    """
-    This class represents PASS's representation of a Person (e.g. PI or Creator).
-    """
-    Can_Edit: Optional[bool] = None
-    Can_Read: Optional[bool] = None
-    CoPI: Optional[bool] = None
-    On_Site: Optional[bool] = None
-    Pool_ID: Optional[int] = None
-    Proposal_ID: Optional[int] = None
-    User_ID: Optional[int] = None
-    Account: Optional[str] = None
-    BNL_ID: Optional[str] = None
-    Email: Optional[str] = None
-    First_Name: Optional[str] = None
-    Last_Name: Optional[str] = None
-    User_Faciity_ID: Optional[str] = None
-
-class PassExperimenter(pydantic.BaseModel):    
+class PassExperimenter(pydantic.BaseModel):
     """
     This class represents PASS's representation of an Experimenter.
     """
+
     Can_Edit: bool
     Can_Read: bool
     CoPI: bool
@@ -82,28 +87,34 @@ class PassExperimenter(pydantic.BaseModel):
     Last_Name: str
     User_Faciity_ID: str
 
+
 class PassResource(pydantic.BaseModel):
     """
     This class represents PASS's representation of a Resource.
     """
+
     ID: int
     Description: str
     User_Facility_ID: str
     Short_Name: str
 
+
 class PassProposalType(pydantic.BaseModel):
     """
     This class represents PASS's representation of a ProposalType.
     """
+
     ID: Optional[int] = None
     Code: Optional[str] = None
     Description: Optional[str] = None
     User_Facility_ID: Optional[str] = None
 
+
 class PassProposal(pydantic.BaseModel):
     """
     This class represents PASS's representation of a Proposal.
     """
+
     Expired: Optional[bool] = None
     Expiration_Date: Optional[str] = None
     Creator_User_ID: Optional[int] = None
@@ -114,16 +125,16 @@ class PassProposal(pydantic.BaseModel):
     Title: Optional[str] = None
     User_Facility_ID: Optional[str] = None
     Creator: Optional[PassPerson] = None
-    PI: Optional[PassPerson]] = None
+    PI: Optional[PassPerson] = None
     Experimenters: Optional[list[PassExperimenter]] = None
     Resources: Optional[list[PassResource]] = None
-
 
 
 class PassScheduledTimeSFTK(pydantic.BaseModel):
     """
     This class represents PASS's representation of a ScheduledTimeSFTK.
     """
+
     ProposalID: Optional[int] = None
     CycleRequestedID: Optional[int] = None
     ResourceID: Optional[int] = None
