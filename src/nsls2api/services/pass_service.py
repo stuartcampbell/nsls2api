4c92969c526dcfe253983d6e79f858373090dd4a
Stuart Campbell <scampbell@bnl.gov>
2024-04-12 17:52:25 -0400

    Refactor PASS service to handle optional facility in get_proposal_types

--- src/nsls2api/services/pass_service.py
+++ src/nsls2api/services/pass_service.py
@@ -1,10 +1,18 @@
 from typing import Optional
+
 from pydantic import ValidationError
+
+from nsls2api.api.models.facility_model import FacilityName
 from nsls2api.infrastructure import config
 from nsls2api.infrastructure.logging import logger
-from nsls2api.services.helpers import _call_async_webservice
+from nsls2api.models.pass_models import (
+    PassCycle,
+    PassProposal,
+    PassProposalType,
+    PassSaf,
+)
 from nsls2api.services import facility_service
-from nsls2api.models.pass_models import PassCycle, PassProposal, PassProposalType
+from nsls2api.services.helpers import _call_async_webservice
 
 settings = config.get_settings()
 
@@ -12,18 +20,35 @@ api_key = settings.pass_api_key
 base_url = settings.pass_api_url
 
 
-async def get_proposal(proposal_id: int) -> Optional[PassProposal]:
-    url = f"{base_url}/Proposal/GetProposal/{api_key}/NSLS-II/{proposal_id}"
+class PassException(Exception):
+    pass
+
+
+async def get_proposal(
+    proposal_id: int, facility: FacilityName = FacilityName.nsls2
+) -> Optional[PassProposal]:
+    pass_facility = await facility_service.pass_id_for_facility(facility)
+
+    if not pass_facility:
+        error_message: str = f"Facility {facility} does not have a PASS ID."
+        logger.error(error_message)
+        raise PassException(error_message)
+
+    url = f"{base_url}/Proposal/GetProposal/{api_key}/{pass_facility}/{proposal_id}"
 
     try:
         pass_proposal = await _call_async_webservice(url)
         proposal = PassProposal(**pass_proposal)
-    except ValidationError:
-        logger.exception("Error validating data recevied from PASS for proposal.")
-        proposal = None
-    except Exception:
-        logger.exception("Error retrieving proposal from PASS.")
-        proposal = None
+    except ValidationError as error:
+        error_message = (
+            f"Error validating data recevied from PASS for proposal {proposal_id}."
+        )
+        logger.error(error_message)
+        raise PassException(error_message) from error
+    except Exception as error:
+        error_message = f"Error retrieving proposal {proposal_id} from PASS."
+        logger.exception(error_message)
+        raise PassException(error_message) from error
 
     return proposal
 
@@ -32,8 +57,9 @@ async def get_proposal_types(facility) -> Optional[PassProposalType]:
     pass_facility = await facility_service.pass_id_for_facility(facility)
 
     if not pass_facility:
-        logger.error(f"Facility {facility} does not have a PASS ID.")
-        return None
+        error_message: str = f"Facility {facility} does not have a PASS ID."
+        logger.error(error_message)
+        raise PassException(error_message)
 
     url = f"{base_url}/Proposal/GetProposalTypes/{api_key}/{pass_facility}"
 
@@ -43,21 +69,47 @@ async def get_proposal_types(facility) -> Optional[PassProposalType]:
         if pass_proposal_types_list and len(pass_proposal_types_list) > 0:
             for proposal_type in pass_proposal_types_list:
                 proposal_types.append(PassProposalType(**proposal_type))
-    except ValidationError:
-        logger.exception("Error validating data recevied from PASS for proposal types.")
-        proposal_types = None
-    except Exception:
-        logger.exception("Error retrieving proposal types from PASS.")
-        proposal_types = None
+    except ValidationError as error:
+        error_message = f"Error validating data recevied from PASS for proposal type for the {facility} facility."
+        logger.error(error_message)
+        raise PassException(error_message) from error
+    except Exception as error:
+        error_message = "Error retrieving proposal types from PASS."
+        logger.error(error_message)
+        raise PassException(error_message) from error
 
     return proposal_types
 
 
-async def get_saf_from_proposal(proposal_id: int):
-    url = f"{base_url}/SAF/GetSAFsByProposal/{api_key}/NSLS-II/{proposal_id}"
+async def get_saf_from_proposal(
+    proposal_id: int, facility: FacilityName = FacilityName.nsls2
+):
+    pass_facility = await facility_service.pass_id_for_facility(facility)
 
-    saf = await _call_async_webservice(url)
-    return saf
+    if not pass_facility:
+        error_message: str = f"Facility {facility} does not have a PASS ID."
+        logger.error(error_message)
+        raise PassException(error_message)
+
+    url = f"{base_url}/SAF/GetSAFsByProposal/{api_key}/{pass_facility}/{proposal_id}"
+    try:
+        pass_saf_list = await _call_async_webservice(url)
+        saf_list = []
+        if pass_saf_list and len(pass_saf_list) > 0:
+            for saf in pass_saf_list:
+                saf_list.append(PassSaf(**saf))
+    except ValidationError as error:
+        error_message = (
+            f"Error validating SAF data recevied from PASS for proposal {proposal_id}."
+        )
+        logger.error(error_message)
+        raise PassException(error_message) from error
+    except Exception as error:
+        error_message = f"Error retrieving SAFs for proposal {proposal_id} from PASS."
+        logger.exception(error_message)
+        raise PassException(error_message) from error
+
+    return saf_list
 
 
 async def get_commissioning_proposals_by_year(year: int):
