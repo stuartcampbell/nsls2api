0bf8d8c9698d761d7af810737e86dabb687ba64c
Stuart Campbell <scampbell@bnl.gov>
2024-04-25 17:45:15 -0400

    refactor: Remove unused import and cleanup code formatting in app_setup.py

--- src/nsls2api/infrastructure/app_setup.py
+++ src/nsls2api/infrastructure/app_setup.py
@@ -1,8 +1,6 @@
 import asyncio
 from contextlib import asynccontextmanager
 
-import httpx
-
 from nsls2api.infrastructure import mongodb_setup
 from nsls2api.infrastructure.config import get_settings
 from nsls2api.services import background_service
@@ -17,7 +15,7 @@ development_mode = True
 @asynccontextmanager
 async def app_lifespan(_):
     if development_mode:
-        # Default to local mongodb with default port 
+        # Default to local mongodb with default port
         # and no authentication for development.
         # TODO: Refactor to use a different database for development.
         await mongodb_setup.init_connection(settings.mongodb_dsn.unicode_string())
@@ -31,7 +29,7 @@ async def app_lifespan(_):
 
     # noinspection PyAsyncCall
     asyncio.create_task(background_service.worker_function())
-    
+
     yield
 
     # Cleanup httpx client
