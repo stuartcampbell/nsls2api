663778ca5082ed76134e7079e72a68078b7e09f1
Stuart Campbell <scampbell@bnl.gov>
2024-04-30 13:01:25 -0400

    Fix some linter warnings

--- src/nsls2api/api/v1/proposal_api.py
+++ src/nsls2api/api/v1/proposal_api.py
@@ -14,17 +14,12 @@ from nsls2api.api.models.proposal_model import (
 )
 from nsls2api.api.models.proposal_model import UsernamesList
 from nsls2api.infrastructure.security import get_current_user
-from nsls2api.models.jobs import (
-    BackgroundJob,
-    JobActions,
-    JobSyncParameters,
-    JobSyncSource,
-)
-from nsls2api.services import background_service, proposal_service
+
+from nsls2api.services import proposal_service
 from nsls2api.api.models.facility_model import FacilityName
 
 
-router = fastapi.APIRouter()
+router = fastapi.APIRouter(dependencies=[Depends(get_current_user)])
 
 
 @router.get("/proposals/recent/{count}", response_model=RecentProposalsList)
@@ -229,68 +224,3 @@ async def get_proposal_directories(proposal_id: int) -> ProposalDirectoriesList:
     )
     return response_model
 
-
-## Sync Endpoints - Will probably move to a more suitable location.
-
-
-@router.get(
-    "/sync/proposal/{proposal_id}",
-    dependencies=[Depends(get_current_user)],
-    include_in_schema=True,
-    tags=["sync"],
-)
-async def sync_proposal(request: Request, proposal_id: int) -> BackgroundJob:
-    sync_params = JobSyncParameters(proposal_id=str(proposal_id))
-    job = await background_service.create_background_job(
-        JobActions.synchronize_proposal,
-        sync_parameters=sync_params,
-    )
-    return job
-
-
-@router.get("/sync/proposal/types/{facility}", include_in_schema=True, tags=["sync"])
-async def sync_proposal_types(facility: FacilityName = FacilityName.nsls2):
-    sync_params = JobSyncParameters(facility=facility)
-    job = await background_service.create_background_job(
-        JobActions.synchronize_proposal_types,
-        sync_parameters=sync_params,
-    )
-    return job
-
-
-@router.get(
-    "/sync/proposals/cycle/{cycle}",
-    dependencies=[Depends(get_current_user)],
-    include_in_schema=True,
-    tags=["sync"],
-)
-async def sync_proposals_for_cycle(request: Request, cycle: str) -> BackgroundJob:
-    sync_params = JobSyncParameters(cycle=cycle)
-    job = await background_service.create_background_job(
-        JobActions.synchronize_proposals_for_cycle,
-        sync_parameters=sync_params,
-    )
-    return job
-
-
-@router.get("/sync/cycles/{facility}", include_in_schema=True, tags=["sync"])
-async def sync_cycles(facility: FacilityName = FacilityName.nsls2):
-    sync_params = JobSyncParameters(facility=facility)
-    job = await background_service.create_background_job(
-        JobActions.synchronize_cycles,
-        sync_parameters=sync_params,
-    )
-    return job
-
-
-@router.get("/sync/update-cycles/{facility}", include_in_schema=True, tags=["sync"])
-async def sync_update_cycles(request : Request, facility: FacilityName = FacilityName.nsls2, cycle : Optional[str] = None):
-
-    sync_params = JobSyncParameters(facility=facility, cycle=cycle, sync_source=JobSyncSource.PASS)
-
-    
-    job = await background_service.create_background_job(
-        JobActions.update_cycle_information,
-        sync_parameters=sync_params,
-    )
-    return job
