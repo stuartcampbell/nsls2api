c2e2c200c080b54056b42b02b1298fe6c4feb3af
Stuart Campbell <scampbell@bnl.gov>
2024-05-09 20:59:01 -0400

    Make testing prefix allowable by slack

--- src/nsls2api/services/proposal_service.py
+++ src/nsls2api/services/proposal_service.py
@@ -87,7 +87,7 @@ def generate_data_session_for_proposal(proposal_id: int) -> str:
 
 def slack_channel_name_for_proposal(proposal_id: str) -> str:
     #TODO: Actually make this configurable and more sensible
-    return f"TEST-SIC-{str(proposal_id)}"
+    return f"test-sic-{str(proposal_id)}"
 
 async def proposal_by_id(proposal_id: int) -> Optional[Proposal]:
     """
