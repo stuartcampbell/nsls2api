3aeaa86dbc3c50faa6c09be0e8ea40663a3fa2eb
Stuart Campbell <scampbell@bnl.gov>
2024-05-10 21:15:47 -0400

    feat: Add SlackUser model
    This commit adds the SlackUser model to the `slack_models.py` file in the `src/nsls2api/models` directory. The model includes fields for `user_id`, `username`, and `email`. This model will be used to represent a Slack user in the application.

--- src/nsls2api/models/slack_models.py
+++ src/nsls2api/models/slack_models.py
@@ -1,6 +1,21 @@
 import pydantic
 
+
 class SlackBot(pydantic.BaseModel):
     username: str
     user_id: str
     bot_id: str
+
+
+class SlackUser(pydantic.BaseModel):
+    user_id: str
+    username: str
+    email: str
+
+
+class SlackChannelCreationResponseModel(pydantic.BaseModel):
+    channel_id: str | None = None
+    channel_name: str | None = None
+    beamline_slack_managers: list[str] | None = []
+    user_ids: list[str] | None = []
+    message: str | None = None
