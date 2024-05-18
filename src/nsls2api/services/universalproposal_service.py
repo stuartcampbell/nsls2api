5ecee90ab4f03ccec3280afdc6e3e3e7573526c7
Stuart Campbell <scampbell@bnl.gov>
2024-05-18 16:57:57 -0400

    Initial version of UPS service

---
+++ src/nsls2api/services/universalproposal_service.py
@@ -0,0 +1,57 @@
+
+
+from nsls2api.api.models.facility_model import UpsFacilityName
+from nsls2api.infrastructure.config import get_settings
+from nsls2api.infrastructure.logging import logger
+
+from nsls2api.services import facility_service
+from nsls2api.services.helpers import _call_async_webservice_with_client
+from nsls2api.infrastructure.app_setup import httpx_client_wrapper
+
+settings = get_settings()
+
+base_url = settings.universal_proposal_system_api_url
+username = settings.universal_proposal_system_api_user
+password = settings.universal_proposal_system_api_password
+
+class UniversalProposalSystemException(Exception):
+    pass
+
+async def _call_ups_servicenow_webservice(url: str):
+    headers = {"Content-Type":"application/json","Accept":"application/json"}
+    return await _call_async_webservice_with_client(url, auth=(username, password), headers=headers, client=httpx_client_wrapper())
+
+
+async def get_proposal_types(
+        facility: UpsFacilityName = UpsFacilityName.nsls2,
+):
+    ups_facility_id = await facility_service.ups_id_for_facility(facility_name=facility)
+
+    if not ups_facility_id:
+        error_message: str = f"Facility {facility} does not have a Universal Proposal System ID."
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message)
+
+    url = f"{base_url}/now/table/sn_customerservice_proposal_type?sysparm_query=u_facility={ups_facility_id}&sysparm_display_value=all"
+
+    ups_proposal_types_list = await _call_ups_servicenow_webservice(url)
+
+    logger.info(f"UPS Returned {len(ups_proposal_types_list)} proposal types.")
+
+    return ups_proposal_types_list["result"]
+
+
+async def get_facility_details(facility: UpsFacilityName = UpsFacilityName.nsls2,):
+    ups_facility_id = await facility_service.ups_id_for_facility(facility_name=facility)
+
+    if not ups_facility_id:
+        error_message: str = f"Facility {facility} does not have a Universal Proposal System ID."
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message)
+    
+    url = f"{base_url}/now/table/cmn_location/{ups_facility_id}"
+
+    ups_facility_info = await _call_ups_servicenow_webservice(url)
+
+    return ups_facility_info["result"]
+
