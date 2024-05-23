3c811a5d92c1ce4f135222b85cb5ad40a12b853c
Stuart Campbell <scampbell@bnl.gov>
2024-05-23 10:18:09 -0400

    Add some error handling

--- src/nsls2api/api/v1/ups_debug_api.py
+++ src/nsls2api/api/v1/ups_debug_api.py
@@ -32,10 +32,32 @@ async def get_ups_facility_info(facility_name: UpsFacilityName = None):
 
 @router.get("/ups/raw/proposal/{proposal_id}")
 async def get_ups_raw_proposal(proposal_id: str):
+    """
+    Retrieves the raw proposal_record from UPS for a given proposal ID.
+
+    Args:
+        proposal_id (str): The ID of the proposal to retrieve.
+
+    Returns:
+        The response from the Service Now Table API for the 'sn_customerservice_proposal_record' table
+    """
     proposal = await universalproposal_service.get_raw_proposal(proposal_id=proposal_id)
     return proposal
 
 @router.get("/ups/proposal/{proposal_id}")
 async def get_ups_proposal(proposal_id: str) -> UpsProposalRecord:
-    proposal = await universalproposal_service.get_proposal(proposal_id=proposal_id)
+    
+    try:
+        proposal = await universalproposal_service.get_proposal(proposal_id=proposal_id)
+        if proposal is None:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=f"Proposal {proposal_id} not found", 
+        )
+    except LookupError as e:
+            raise fastapi.HTTPException(
+                status_code=fastapi.status.HTTP_404_NOT_FOUND,
+                detail=e.args[0], 
+        )
+    # response_model = SingleProposal(proposal=proposal)
     return proposal
