2fb42e1105bf0f8b1f8439fca451f359227dd249
Stuart Campbell <scampbell@bnl.gov>
2024-05-28 18:22:13 -0400

    Add method to get all proposals for a facility

--- src/nsls2api/services/universalproposal_service.py
+++ src/nsls2api/services/universalproposal_service.py
@@ -1,8 +1,8 @@
 from typing import Optional
 
 from pydantic import ValidationError
-from nsls2api.api.models.facility_model import UpsFacilityName
-from nsls2api.models.universalproposal_models import UpsProposalType, UpsProposalRecord
+from nsls2api.api.models.facility_model import FacilityName, UpsFacilityName
+from nsls2api.models.universalproposal_models import UpsCycle, UpsProposalType, UpsProposalRecord
 from nsls2api.infrastructure.config import get_settings
 from nsls2api.infrastructure.logging import logger
 
@@ -154,3 +154,90 @@ async def get_proposal(proposal_id: str) -> Optional[UpsProposalRecord]:
         raise UniversalProposalSystemException(error_message) from error
 
     return proposal
+
+async def get_all_proposals_for_facility(facility: FacilityName = FacilityName.nsls2) -> Optional[list[UpsProposalRecord]]:
+    
+    # First check we have the details for the facility we need.
+    ups_facility_id = await facility_service.ups_id_for_facility(facility_name=facility)
+    if not ups_facility_id:
+        error_message: str = (
+            f"Facility {facility} does not have a Universal Proposal System ID."
+        )
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message)
+    
+    # Get a list of proposal types for the given facility
+    proposal_types = await get_proposal_types(facility=facility)
+
+    if not proposal_types or len(proposal_types) == 0:
+        error_message: str = (
+            f"Facility {facility} does not have any proposal types within the UPS."
+        )
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message)
+
+    facility_proposal_list =[]    
+
+    servicenow_table_name = "sn_customerservice_proposal_record"
+
+    for proposal_type in proposal_types:
+        query=f"u_proposal_type%3D{proposal_type.sys_id.value}"
+        url = f"{base_url}/now/table/{servicenow_table_name}?sysparm_query={query}&sysparm_display_value=all"
+
+        try:
+            ups_proposal_response = await _call_ups_servicenow_webservice(url)
+            ups_proposals = ups_proposal_response["result"]
+            if ups_proposals and len(ups_proposals) > 0:
+                logger.info(f"Found {len(ups_proposals)} proposals for proposal type {proposal_type.u_name.value}.")
+                for ups_proposal in ups_proposals:
+                    # logger.info(f"Validating proposal {ups_proposal['u_proposal_number']}.")
+                    facility_proposal_list.append(UpsProposalRecord(**ups_proposal))
+        except ValidationError as error:
+            error_message = (
+                f"Error validating data recevied from UPS for all proposals from {facility}."
+            )
+            logger.error(error_message)
+            raise UniversalProposalSystemException(error_message) from error
+        except Exception as error:
+            error_message = f"Error retrieving proposals from UPS. {error}"
+            logger.exception(error_message)
+            raise UniversalProposalSystemException(error_message) from error
+
+    return facility_proposal_list
+
+
+async def get_cycles(
+    facility: FacilityName = FacilityName.nsls2,
+) -> Optional[list[UpsCycle]]:
+    ups_facility_id = await facility_service.ups_id_for_facility(facility_name=facility)
+
+    if not ups_facility_id:
+        error_message: str = (
+            f"Facility {facility} does not have a Universal Proposal System ID."
+        )
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message)
+
+    servicenow_table_name = "sn_customerservice_run_cycle"
+    url = f"{base_url}/now/table/{servicenow_table_name}?sysparm_query=u_facility={ups_facility_id}&sysparm_display_value=all"
+
+    logger.info(f"Getting cycles from UPS for {facility} facility.")
+
+    try:
+        ups_cycle_list_response = await _call_ups_servicenow_webservice(url)
+        ups_cycle_list = ups_cycle_list_response["result"]
+        cycles = []
+        if ups_cycle_list and len(ups_cycle_list) > 0:
+            for ups_cycle in ups_cycle_list:
+                cycles.append(UpsCycle(**ups_cycle))
+    except ValidationError as error:
+        error_message = f"Error validating cycle data recevied from UPS for the {facility} facility."
+        logger.error(error_message)
+        raise UniversalProposalSystemException(error_message) from error
+    except Exception as error:
+        error_message = "Error retrieving cycle information from UPS."
+        logger.exception(error_message)
+        raise UniversalProposalSystemException(error_message) from error
+
+    return cycles
+
