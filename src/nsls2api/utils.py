f513856f800eb77623abed19289050b47dc855c5
Stuart Campbell <scampbell@bnl.gov>
2024-05-28 18:20:55 -0400

    Add utility function to convert string to boolean

---
+++ src/nsls2api/utils.py
@@ -0,0 +1,14 @@
+
+def string_to_bool (value: str) -> bool:
+    """Convert a string representation of truth to boolean.
+    True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
+    are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
+    'val' is anything else.
+    """
+    value = value.lower()
+    if value in ('y', 'yes', 't', 'true', 'on', '1'):
+        return True
+    elif value in ('n', 'no', 'f', 'false', 'off', '0'):
+        return False
+    else:
+        raise ValueError("invalid truth value %r" % (value,))
