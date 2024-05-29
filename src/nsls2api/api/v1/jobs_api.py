b4f2d76f6a958a249f3e0df5698e31017b1c1b1a
Stuart Campbell <scampbell@bnl.gov>
2024-05-29 15:36:28 -0400

    Add sync source parameters to endpoints

--- src/nsls2api/api/v1/jobs_api.py
+++ src/nsls2api/api/v1/jobs_api.py
@@ -43,8 +43,12 @@ async def check_job_status(request: Request, job_id: str):
     include_in_schema=SYNC_ROUTES_IN_SCHEMA,
     tags=["sync"],
 )
-async def sync_proposal(request: Request, proposal_id: int) -> BackgroundJob:
-    sync_params = JobSyncParameters(proposal_id=str(proposal_id))
+async def sync_proposal(
+    request: Request, proposal_id: str, sync_source: JobSyncSource = JobSyncSource.PASS
+) -> BackgroundJob:
+    sync_params = JobSyncParameters(
+        proposal_id=str(proposal_id), sync_source=sync_source
+    )
     job = await background_service.create_background_job(
         JobActions.synchronize_proposal,
         sync_parameters=sync_params,
@@ -75,8 +79,10 @@ async def sync_proposal_types(
     include_in_schema=SYNC_ROUTES_IN_SCHEMA,
     tags=["sync"],
 )
-async def sync_proposals_for_cycle(request: Request, cycle: str) -> BackgroundJob:
-    sync_params = JobSyncParameters(cycle=cycle)
+async def sync_proposals_for_cycle(
+    request: Request, cycle: str, sync_source: JobSyncSource = JobSyncSource.PASS
+) -> BackgroundJob:
+    sync_params = JobSyncParameters(cycle=cycle, sync_source=sync_source)
     job = await background_service.create_background_job(
         JobActions.synchronize_proposals_for_cycle,
         sync_parameters=sync_params,
@@ -93,6 +99,15 @@ async def sync_proposals_for_cycle(request: Request, cycle: str) -> BackgroundJo
 async def sync_all_proposals_for_facility(
     request: Request, facility: FacilityName = FacilityName.nsls2
 ) -> BackgroundJob:
+    """
+    Synchronizes all proposals for a given facility.  This only uses the Universal Proposal System as the source.
+
+    Args:
+        facility (FacilityName, optional): The facility name. Defaults to FacilityName.nsls2.
+
+    Returns:
+        BackgroundJob: The background job for the synchronization process.
+    """
     sync_params = JobSyncParameters(
         facility=facility, sync_source=JobSyncSource.universal_proposal_system
     )
@@ -127,10 +142,10 @@ async def sync_update_cycles(
     request: fastapi.Request,
     facility: FacilityName = FacilityName.nsls2,
     cycle: Optional[str] = None,
-    sync_source: JobSyncSource = JobSyncSource.PASS,
 ):
     sync_params = JobSyncParameters(
-        facility=facility, cycle=cycle, sync_source=sync_source
+        facility=facility,
+        cycle=cycle,
     )
 
     job = await background_service.create_background_job(
