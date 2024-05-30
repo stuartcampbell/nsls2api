e4ae3dfbd17b93d8526e61c00e03733262b002b1
Stuart Campbell <scampbell@bnl.gov>
2024-05-30 12:05:04 -0400

    correct typo

--- src/nsls2api/models/facilities.py
+++ src/nsls2api/models/facilities.py
@@ -11,7 +11,7 @@ class Facility(beanie.Document):
     fullname: str
     pass_facility_id: Optional[str] = None
     data_admins: Optional[list[str]] = []
-    data_admin_group: Optional
+    data_admin_group: Optional[str] = None
     created_on: datetime.datetime = pydantic.Field(
         default_factory=datetime.datetime.now
     )
