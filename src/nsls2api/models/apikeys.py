3f7052bf8b984be5bba76ad14a1c5a93f6c1d940
Stuart Campbell <scampbell@bnl.gov>
2024-06-07 13:28:34 -0400

    Changed to using StrEnum and added an ApiUser Response model

--- src/nsls2api/models/apikeys.py
+++ src/nsls2api/models/apikeys.py
@@ -1,5 +1,5 @@
 import datetime
-import enum
+from enum import StrEnum
 from typing import Optional, List
 from uuid import UUID, uuid4
 
@@ -10,15 +10,23 @@ from beanie import Link, BackLink
 from pydantic import Field
 
 
-class ApiUserType(str, enum.Enum):
+class ApiUserType(StrEnum):
     user = "user"
     service = "service"
 
 
-class ApiUserRole(str, enum.Enum):
+class ApiUserRole(StrEnum):
     user = "user"
     staff = "staff"
     admin = "admin"
+
+class ApiUserResponseModel(pydantic.BaseModel):
+    id: UUID
+    username: str
+    type: ApiUserType
+    role: ApiUserRole
+    created_on: datetime.datetime
+    last_updated: datetime.datetime
 
 
 class ApiUser(beanie.Document):
@@ -60,7 +68,9 @@ class ApiKey(beanie.Document):
     user: Link[ApiUser]
     username: str
     first_eight: pydantic.constr(min_length=8, max_length=8)
-    secret_key: str  # TODO: After development - we will not be storing this one in the database
+    secret_key: (
+        str  # TODO: After development - we will not be storing this one in the database
+    )
     hashed_key: str
     note: Optional[str] = ""
     # scopes: Optional[list[str]] = pydantic.Field(..., example=["inherit"])
