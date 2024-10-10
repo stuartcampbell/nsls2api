4ef50802a0586b4352bf8c6d047f227c134613a3
Stuart Campbell <scampbell@bnl.gov>
2024-10-10 11:35:20 -0400

    Add initial infrastructure tests

--- src/nsls2api/tests/api/conftest.py
+++ src/nsls2api/tests/api/conftest.py
@@ -1,15 +1,15 @@
 import pytest
 from asgi_lifespan import LifespanManager
-from httpx import AsyncClient
+from httpx import AsyncClient, ASGITransport
 
 from nsls2api.main import app
 from nsls2api.models import facilities, jobs, apikeys, proposals, beamlines, cycles, proposal_types
 
-@pytest.fixture()
+@pytest.fixture(scope="session", autouse=True)
 async def test_client(db):
     async with LifespanManager(app, startup_timeout=100, shutdown_timeout=100):
         server_name = "http://localhost"
-        async with AsyncClient(app=app, base_url=server_name, follow_redirects=True) as client:
+        async with AsyncClient(transport=ASGITransport(app=app), base_url=server_name, follow_redirects=True) as client:
             yield client
 
 #
--- src/nsls2api/tests/conftest.py
+++ src/nsls2api/tests/conftest.py
@@ -1,19 +1,19 @@
-import motor.motor_asyncio
+import asyncio
+
 import pytest
+from pydantic import MongoDsn
 from pydantic_settings import BaseSettings
 
+from nsls2api.infrastructure.mongodb_setup import create_connection_string, init_connection
 
-class Settings(BaseSettings):
-    mongodb_dsn: str = "mongodb://localhost:27017/test_db"
-    mongodb_db_name: str = "test_db"
+@pytest.fixture(scope="session")
+def event_loop():
+    return asyncio.get_event_loop()
 
-
-@pytest.fixture
-def settings():
-    return Settings()
-
-@pytest.fixture
-def db(settings: Settings) -> motor.motor_asyncio.AsyncIOMotorDatabase:
-    client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_dsn)
-    print(f'client: {client[settings.mongodb_db_name]}')
-    return client[settings.mongodb_db_name]
+@pytest.fixture(scope="session", autouse=True)
+async def db():
+    mongodb_dsn = create_connection_string(host="localhost", port=27017, db_name="test_db")
+    await init_connection(mongodb_dsn.unicode_string())
+    # client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_dsn)
+    # print(f'client: {client[settings.mongodb_db_name]}')
+    # return client[settings.mongodb_db_name]
