b645a0eb16767ea0cf388340aa076a093e2b186b
Stuart Campbell <scampbell@bnl.gov>
2024-10-10 16:58:12 -0400

    Populate database and alter tests to match

--- src/nsls2api/tests/api/test_beamline_api.py
+++ src/nsls2api/tests/api/test_beamline_api.py
@@ -1,43 +1,41 @@
 import pytest
 from httpx import AsyncClient
 from nsls2api.models.beamlines import ServiceAccounts, Beamline
-from nsls2api.models.validation_error import ValidationError
-
 
 async def test_get_beamline_service_accounts(test_client: AsyncClient):
-    response = await test_client.get("/v1/beamline/tst/service-accounts")
+    response = await test_client.get("/v1/beamline/zzz/service-accounts")
     response_json = response.json()
     assert response.status_code == 200
 
     # Make sure we can create a ServiceAccounts object from the response
     accounts = ServiceAccounts(**response_json)
-    assert accounts.workflow == "workflow-tst"
-    assert accounts.ioc == "softioc-tst"
-    assert accounts.bluesky == "bluesky-tst"
-    assert accounts.epics_services == "epics-services-tst"
-    assert accounts.operator == "xf31id"
+    assert accounts.workflow == "testy-mctestface-workflow"
+    assert accounts.ioc == "testy-mctestface-ioc"
+    assert accounts.bluesky == "testy-mctestface-bluesky"
+    assert accounts.epics_services == "testy-mctestface-epics-services"
+    assert accounts.operator == "testy-mctestface-xf66id6"
     assert accounts.lsdc is None or accounts.lsdc == ""
 
 
 async def test_get_beamline_lowercase(test_client: AsyncClient):
-    response = await test_client.get("/v1/beamline/tst")
+    response = await test_client.get("/v1/beamline/zzz")
     response_json = response.json()
     assert response.status_code == 200
 
     # Make sure we can create a Beamline object from the response
     beamline = Beamline(**response_json)
-    assert beamline.name == "TST"
+    assert beamline.name == "ZZZ"
 
 async def test_get_beamline_uppercase(test_client: AsyncClient):
-    response = await test_client.get("/v1/beamline/TST")
+    response = await test_client.get("/v1/beamline/ZZZ")
     response_json = response.json()
     assert response.status_code == 200
 
     # Make sure we can create a Beamline object from the response
     beamline = Beamline(**response_json)
-    assert beamline.name == "TST"
+    assert beamline.name == "ZZZ"
 
 async def test_get_beamline_directory_skeleton(test_client: AsyncClient):
-    response = await test_client.get("/v1/beamline/tst/directory-skeleton")
+    response = await test_client.get("/v1/beamline/zzz/directory-skeleton")
     response_json = response.json()
     assert response.status_code == 200
--- src/nsls2api/tests/conftest.py
+++ src/nsls2api/tests/conftest.py
@@ -1,19 +1,47 @@
 import asyncio
-
 import pytest
-from pydantic import MongoDsn
-from pydantic_settings import BaseSettings
 
-from nsls2api.infrastructure.mongodb_setup import create_connection_string, init_connection
+from nsls2api.infrastructure.config import get_settings
+from nsls2api.infrastructure.mongodb_setup import init_connection
+from nsls2api import models
+from nsls2api.models.beamlines import Beamline, ServiceAccounts
+from nsls2api.models.facilities import Facility
+from nsls2api.services.beamline_service import service_accounts
+
 
 @pytest.fixture(scope="session")
 def event_loop():
     return asyncio.get_event_loop()
 
-@pytest.fixture(scope="session", autouse=True)
+@pytest.fixture(scope="session")
 async def db():
-    mongodb_dsn = create_connection_string(host="localhost", port=27017, db_name="test_db")
-    await init_connection(mongodb_dsn.unicode_string())
-    # client = motor.motor_asyncio.AsyncIOMotorClient(settings.mongodb_dsn)
-    # print(f'client: {client[settings.mongodb_db_name]}')
-    # return client[settings.mongodb_db_name]
+
+    settings = get_settings()
+    await init_connection(settings.mongodb_dsn.unicode_string())
+
+    # Insert a beamline into the database
+    beamline = Beamline(name="ZZZ", port="66-ID-6", long_name="Magical PyTest X-Ray Beamline",
+                        alternative_name="66-ID", network_locations=["xf66id6"],
+                        pass_name="Beamline 66-ID-6", pass_id="666666",
+                        nsls2_redhat_satellite_location_name="Nowhere",
+                        service_accounts=ServiceAccounts(ioc="testy-mctestface-ioc",
+                                                         bluesky="testy-mctestface-bluesky",
+                                                         epics_services="testy-mctestface-epics-services",
+                                                         operator="testy-mctestface-xf66id6",
+                                                         workflow="testy-mctestface-workflow")
+                        )
+    await beamline.insert()
+
+    # Insert a facility into the database
+    facility = Facility(name="NSLS-II", facility_id="nsls2",
+                        fullname="National Synchrotron Light Source II",
+                        pass_facility_id="NSLS-II")
+    await facility.insert()
+
+    yield
+
+    # Cleanup the database collections
+    for model in models.all_models:
+        print(f"dropping {model}")
+        await model.get_motor_collection().drop()
+        await model.get_motor_collection().drop_indexes()
