0eea6f761503f3a159895d576255604d5a9a57e2
Stuart Campbell <scampbell@bnl.gov>
2024-10-16 08:41:43 -0400

    Reformat

--- src/nsls2api/tests/test_home.py
+++ src/nsls2api/tests/test_home.py
@@ -4,8 +4,8 @@ from nsls2api.main import app
 
 client = TestClient(app)
 
+
 def test_healthy_endpoint():
     response = client.get("/healthy")
     assert response.status_code == 200
     assert response.text == "OK"
-
