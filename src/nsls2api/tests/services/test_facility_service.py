c3f9210cda4693c62b33ba49ed2932b2e4bf416d
Stuart Campbell <scampbell@bnl.gov>
2024-10-21 14:47:41 -0400

    Add tests for facility_service

---
+++ src/nsls2api/tests/services/test_facility_service.py
@@ -0,0 +1,14 @@
+import pytest
+
+from nsls2api.services import facility_service
+
+
+@pytest.mark.anyio
+async def test_facilities_count():
+    assert await facility_service.facilities_count() == 1
+
+
+@pytest.mark.anyio
+async def test_all_facilities():
+    facilities = await facility_service.all_facilities()
+    assert type(facilities) == list
