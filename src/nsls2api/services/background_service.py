aba9245377c417cb9c68a2816213561f1b171d01
Stuart Campbell <scampbell@bnl.gov>
2025-01-09 09:58:18 -0500

    Fix misuse of english.

--- src/nsls2api/services/background_service.py
+++ src/nsls2api/services/background_service.py
@@ -130,7 +130,7 @@ async def worker_function():
                     )
                 case JobActions.synchronize_proposals_for_cycle:
                     logger.info(
-                        f"Processing job {job.id} to synchronize proposals for the {job.sync_parameters.facility} facilities cycle {job.sync_parameters.cycle} (from {job.sync_parameters.sync_source})."
+                        f"Processing job {job.id} to synchronize proposals for the {job.sync_parameters.facility} facility's cycle {job.sync_parameters.cycle} (from {job.sync_parameters.sync_source})."
                     )
                     await sync_service.worker_synchronize_proposals_for_cycle_from_pass(
                         job.sync_parameters.cycle, job.sync_paramters.facility
