6c3f7a68d68ebd1eccae4d8dfd6c72a454af5c79
Stuart Campbell <scampbell@bnl.gov>
2025-03-11 17:47:59 -0400

    Move Slack channel creation endpoints to proposal api and clean up imports

--- src/nsls2api/api/v1/admin_api.py
+++ src/nsls2api/api/v1/admin_api.py
@@ -6,19 +6,17 @@ from fastapi import Depends, HTTPException
 from nsls2api.api.models.facility_model import FacilityName
 from nsls2api.api.models.proposal_model import SingleProposal
 from nsls2api.infrastructure import config
-from nsls2api.infrastructure.logging import logger
 from nsls2api.infrastructure.security import (
-    validate_admin_role,
     generate_api_key,
+    validate_admin_role,
 )
 from nsls2api.models.apikeys import (
     ApiUser,
-    ApiUserRole,
     ApiUserResponseModel,
+    ApiUserRole,
     ApiUserType,
 )
-from nsls2api.models.slack_models import SlackChannelResponseModel, SlackChannelResponseModelList
-from nsls2api.services import beamline_service, proposal_service, slack_service
+from nsls2api.services import proposal_service
 
 # router = fastapi.APIRouter()
 router = fastapi.APIRouter(
@@ -76,92 +74,6 @@ async def generate_fake_proposal(
 
     return SingleProposal(proposal=proposal)
 
-@router.post("/admin/proposal/{proposal_id}/slack-channels")
-async def create_slack_channels_for_proposal(proposal_id: str) -> list[SlackChannelResponseModel]:
-    pass
-
-@router.get("/admin/proposal/{proposal_id}/slack-channels")
-async def get_slack_channels_for_proposal(proposal_id: str) -> SlackChannelResponseModelList:
-    pass
-
-@router.post("/admin/slack/create-proposal-channel/{proposal_id}")
-async def create_slack_channel(proposal_id: str) -> SlackChannelResponseModel:
-    proposal = await proposal_service.proposal_by_id(proposal_id)
-
-    if proposal is None:
-        raise HTTPException(
-            status_code=fastapi.status.HTTP_404_NOT_FOUND,
-            detail=f"Proposal {proposal_id} not found",
-        )
-
-    channel_name = proposal_service.legacy_generate_slack_channel_name_for_proposal(proposal_id)
-
-    if channel_name is None:
-        raise HTTPException(
-            status_code=fastapi.status.HTTP_422_UNPROCESSABLE_ENTITY,
-            detail=f"Slack channel name could not be generated for the proposal {proposal_id}",
-        )
-
-    channel_id = await slack_service.create_channel(
-        channel_name,
-        is_private=True,
-    )
-
-    if channel_id is None:
-        raise HTTPException(
-            status_code=fastapi.status.HTTP_500_INTERNAL_SERVER_ERROR,
-            detail=f"Slack channel could not be created for the proposal {proposal_id}",
-        )
-
-    logger.info(f"Created slack channel '{channel_name}' for proposal {proposal_id}.")
-
-    # Store the created Slack channel ID
-    proposal.slack_channel_id = channel_id
-    await proposal.save()
-
-    # Add the beamline Slack channel managers to the channel
-    slack_managers_added = []
-    for beamline in proposal.instruments:
-        slack_managers = await beamline_service.slack_channel_managers(beamline)
-        logger.info(
-            f"Adding Slack channel managers for {beamline} beamline [{slack_managers}]."
-        )
-        if len(slack_managers) > 0:
-            slack_service.add_users_to_channel(
-                channel_id=channel_id, user_ids=slack_managers
-            )
-            slack_managers_added.append(slack_managers)
-
-    # Add the users on the proposal to the channel
-    proposal_user_ids = []
-    for user in proposal.users:
-        # If username is populated then user has an account
-        if user.username is not None:
-            user_slack_id = slack_service.lookup_userid_by_email(user.email)
-            if user_slack_id is None:
-                logger.info(f"User {user.username} does not have a slack_id")
-            else:
-                logger.info(
-                    f"Adding user {user.username} ({user_slack_id}) to slack channel..."
-                )
-                proposal_user_ids.append(user_slack_id)
-
-    logger.info(
-        f"Slack users {proposal_user_ids} will be added to the proposal channel"
-    )
-
-    # TODO: Uncomment to actually add the users when we are sure!!
-    # slack_service.add_users_to_channel(channel_id=channel_id, user_ids=proposal_user_ids)
-
-    response_model = SlackChannelResponseModel(
-        channel_id=channel_id,
-        channel_name=channel_name,
-        beamline_slack_managers=slack_managers_added,
-        user_ids=proposal_user_ids,
-    )
-
-    return response_model
-
 
 @router.put("/admin/user/{username}/role/{role}")
 async def update_user_role(username: str, role: ApiUserRole) -> ApiUserResponseModel:
@@ -180,7 +92,7 @@ async def update_user_role(username: str, role: ApiUserRole) -> ApiUserResponseM
         )
 
     user.role = role
-    await user.save()
+    await user.save()  # noqa
 
     response = ApiUserResponseModel(
         id=user.id,
