56a8f04e58cd935b632de00fbbe415d3c2cd5df0
Stuart Campbell <scampbell@bnl.gov>
2025-03-11 11:56:13 -0400

    Refactor Slack models to introduce SlackPerson and enhance SlackChannel with new fields for proposals

--- src/nsls2api/models/slack_models.py
+++ src/nsls2api/models/slack_models.py
@@ -1,25 +1,34 @@
-from typing import List
 
 import pydantic
-
-
-class SlackBot(pydantic.BaseModel):
-    username: str
-    user_id: str
-    bot_id: str
 
 
 class SlackUser(pydantic.BaseModel):
     user_id: str
     username: str
+
+class SlackPerson(SlackUser):
     email: str
+
+class SlackBot(SlackUser):
+    bot_id: str
 
 class SlackChannel(pydantic.BaseModel):
     channel_id: str
     channel_name: str
-    users: List[SlackUser]
-    created: bool
+
+class SlackChannelToCreate(pydantic.BaseModel):
+    channel_name: str
+    proposal_id: str
+    beamlines: list[str] | None = None
+    topic: str | None = None
+
+class ProposalSlackChannel(SlackChannel):
+    topic: str | None = None
+    users: list[SlackUser] | None = None
+    managers: list[SlackUser] | None = None
+    bots: list[str] | None = None
     is_private: bool
+
 
 class SlackChannelResponseModel(pydantic.BaseModel):
     channel_id: str
@@ -28,6 +37,7 @@ class SlackChannelResponseModel(pydantic.BaseModel):
     user_ids: list[str] | None = []
     message: str | None = None
 
-class SlackChannelResponseModelList(pydantic.BaseModel):
-    slack_channels: list[SlackChannelResponseModel]
+
+class ProposalSlackChannelList(pydantic.BaseModel):
+    slack_channels: list[ProposalSlackChannel]
     count: int
