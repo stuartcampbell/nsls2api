23dd93a73e408bae1c8428514a0eaea5d96ad477
Stuart Campbell <scampbell@bnl.gov>
2025-03-31 13:15:37 -0400

    Prevent inviting empty user lists to Slack channels

--- src/nsls2api/services/slack_service.py
+++ src/nsls2api/services/slack_service.py
@@ -431,7 +431,9 @@ async def create_proposal_channels(
         logger.info(
             f"Found {len(user_ids)} users to invite: {user_ids} to channel '{channel_name}' (ID: {channel_id})"
         )
-        conversation_invite(channel_id, user_ids)
+        if len(user_ids) > 0:
+            # If we don't have any users to invite - then don't invite them.
+            conversation_invite(channel_id, user_ids)
 
         channels_created.append(proposal_channel)
 
