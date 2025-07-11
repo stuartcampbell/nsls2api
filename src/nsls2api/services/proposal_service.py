45a4b6f4a5b8693dbe2b7eeaf8117a9c67fd4283
sjulian4 <sarah.julian@stonybrook.edu>
2025-07-11 16:09:42 -0400

    adding proposal not found httpexception

--- src/nsls2api/services/proposal_service.py
+++ src/nsls2api/services/proposal_service.py
@@ -87,9 +87,6 @@ async def lock(proposal_list: ProposalsToChangeLockedStatus) -> ProposalLockingR
                 proposal_object.locked = True
                 await proposal_object.save()  # Save the updated proposal object
                 successfully_locked_proposals.append(proposal_id)
-        except LookupError:
-            failed_to_lock_proposals.append(proposal_id)
-            logger.info(f"Proposal {proposal_id} not found")
         except Exception as e:
             failed_to_lock_proposals.append(proposal_id)
             logger.info(
@@ -119,9 +116,6 @@ async def unlock(proposal_list: ProposalsToChangeLockedStatus) -> ProposalLockin
                 proposal_object.locked = False
                 await proposal_object.save()  # Save the updated proposal object
                 successfully_unlocked_proposals.append(proposal_id)
-        except LookupError:
-            failed_to_unlock_proposals.append(proposal_id)
-            logger.info(f"Proposal {proposal_id} not found")
         except Exception as e:
             failed_to_unlock_proposals.append(proposal_id)
             logger.info(
