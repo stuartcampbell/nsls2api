403d794eefbc7897dd9f989f02e00c3a3e7910a0
Sarah Julian <sjulian@bnl.gov>
2025-07-31 10:03:03 -0400

    Adding case sensitivity test

---
+++ src/nsls2api/tests/api/test_proposal_api.py
@@ -0,0 +1,21 @@
+import pytest
+
+from nsls2api.services import proposal_service
+
+test_beamline_name = "ZZZ"
+
+test_beamline_name_lower = "zzz"
+
+test_proposal_id = "314159"
+
+@pytest.mark.anyio
+async def test_case_sensitivity_fetch_proposals():
+    proposal_objects_upper = await proposal_service.fetch_proposals(
+        proposal_id=[test_beamline_name]
+    )
+    proposal_objects_lower = await proposal_service.fetch_proposals(
+        proposal_id=[test_beamline_name_lower]
+    )
+
+    assert proposal_objects_upper == proposal_objects_lower
+    assert proposal_objects_lower.proposal_id[0] == test_proposal_id
